24f980f7986f9df73a544bc5d940bc2a722b1f13
Matis Byar <matis.byar.06@icloud.com>
2024-11-07 10:11:38 +0100

    Created gRPC files for Times service

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -1,18 +1,31 @@
-import grpc
+import json
 from concurrent import futures
+
+import grpc
+
 import showtime_pb2
 import showtime_pb2_grpc
-import json
 
-class ShowtimeServicer(showtime_pb2_grpc.ShowtimeServicer):
+
+class ShowtimeServicer(showtime_pb2_grpc.TimesServicer):
 
     def __init__(self):
         with open('{}/data/times.json'.format("."), "r") as jsf:
             self.db = json.load(jsf)["schedule"]
 
+    def GetShowtimes(self, request, context):
+        for schedule in self.db:
+            yield showtime_pb2.ShowtimeData(date=schedule['date'], movie=schedule['movie'])
+
+    def GetShowtimeByDate(self, request, context):
+        for schedule in self.db:
+            if schedule['date'] == request.date:
+                return showtime_pb2.ShowtimeData(date=schedule['date'], movie=schedule['movie'])
+        return showtime_pb2.ShowtimeData(date="", movie=[])
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    showtime_pb2_grpc.add_ShowtimeServicer_to_server(ShowtimeServicer(), server)
+    showtime_pb2_grpc.add_TimesServicer_to_server(ShowtimeServicer(), server)
     server.add_insecure_port('[::]:3002')
     server.start()
     server.wait_for_termination()
